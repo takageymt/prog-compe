c53fae408039e937d45299d1c723238c81f9716f
Takuma Kageyama <uku09unk@gmail.com>
2016-12-19 19:11:10 +0900

    update template

--- Geometry/template.cpp
+++ Geometry/template.cpp
@@ -1,14 +1,15 @@
 #define EPS (1e-10)
 #define equals(a, b) (fabs((a)-(b)) < EPS)
 
+// 点/ベクトル
 struct Point {
   double x, y;
   Point(double x = 0.0, double y = 0.0):x(x), y(y){}
-
+  
   Point operator + (Point p) { return Point(x + p.x, y + p.y); }
   Point operator - (Point p) { return Point(x - p.x, y - p.y); }
-  Point operator * (double a) { return Point(x*a, y*a); }
-  Point operator / (double a) { return Point(x/a, y/a); }
+  Point operator * (double a) { return Point(x * a, y * a); }
+  Point operator / (double a) { return Point(x / a, y / a); }
 
   double abs() { return sqrt(norm()); }
   double norm() { return x*x + y*y; }
@@ -20,23 +21,45 @@ struct Point {
     return fabs(x - p.x) < EPS && fabs(y - p.y) < EPS;
   }
 };
-
 typedef Point Vector;
 
+// 円
+struct Circle {
+  Point c;
+  double r;
+  Circle(Point c = Point(), double r = 0.0):c(c), r(r){}
+};
+
+// 多角形
+typedef vector<Point> Polygon;
+
+// 線分/直線
 struct Segment {
   Point p1, p2;
 };
-
 typedef Segment Line;
 
+// ベクトルのノルム
+double norm(Vector v)
+{
+  return v.x*v.x + v.y*v.y;
+}
+// ベクトルの大きさ
+double abs(Vector v)
+{
+  return sqrt(norm(v));
+}
+// ベクトルの内積
 double dot(Vector a, Vector b)
 {
   return a.x*b.x + a.y*b.y;
 }
+// ベクトルの外積の大きさ
 double cross(Vector a, Vector b)
 {
   return a.x*b.y - a.y*b.x;
 }
+// 直行判定
 bool isOrthogonal(Vector a, Vector b)
 {
   return equals(dot(a, b), 0.0);
@@ -49,6 +72,7 @@ bool isOrthogonal(Segment s1, Segment s2)
 {
   return equals(dot(s1.p2 - s1.p1, s2.p2 - s2.p1), 0.0);
 }
+// 平行判定
 bool isParallel(Vector a, Vector b)
 {
   return equals(cross(a, b), 0.0);
@@ -61,41 +85,140 @@ bool isParallel(Segment s1, Segment s2)
 {
   return equals(cross(s1.p2 - s1.p1, s2.p2 - s2.p1), 0.0);
 }
+// 射影
 Point project(Segment s, Point p)
 {
   Vector base = s.p2 - s.p1;
   double r = dot(p - s.p1, base) / norm(base);
   return s.p1 + base * r;
 }
+// 反射
 Point reflect(Segment s, Point p)
 {
   return p + (project(s, p) - p) * 2.0;
 }
+static const int COUNTER_CLOCKWISE = 1;
+static const int CLOCKWISE         = -1;
+static const int ONLINE_BACK       = 2;
+static const int ONLINE_FRONT      = -2;
+static const int ON_SEGMENT        = 0;
+// 反時計回り
+int ccw(Point p0, Point p1, Point p2)
+{
+  Vector a = p1 - p0;
+  Vector b = p2 - p0;
+  if(cross(a, b) > EPS) return COUNTER_CLOCKWISE;
+  if(cross(a, b) < -EPS) return CLOCKWISE;
+  if(dot(a, b) < -EPS) return ONLINE_BACK;
+  if(a.norm() < b.norm()) return ONLINE_FRONT;
+  return ON_SEGMENT;
+}
+// 交差判定
+bool intersect(Point p1, Point p2, Point p3, Point p4)
+{
+  return (ccw(p1, p2, p3) * ccw(p1, p2, p4) <= 0 &&
+	  ccw(p3, p4, p1) * ccw(p3, p4, p2) <= 0);
+}
+bool intersect(Segment s1, Segment s2)
+{
+  return intersect(s1.p1, s1.p2, s2.p1, s2.p2);
+}
+// 二点間の距離
 double getDistance(Point a, Point b)
 {
   return abs(a - b);
 }
+// 直線と点との距離
 double getDistanceLP(Line l, Point p)
 {
   return abs(cross(l.p2 - l.p1, p - l.p1) / abs(l.p2 - l.p1));
 }
+// 線分と点との距離
 double getDistanceSP(Segment s, Point p)
 {
   if(dot(s.p2 - s.p1, p - s.p1) < 0.0) return abs(p - s.p1);
   if(dot(s.p1 - s.p2, p - s.p2) < 0.0) return abs(p - s.p2);
   return getDistanceLP(s, p);
 }
+// 二線分間の距離
 double getDistance(Segment s1, Segment s2)
 {
   if(intersect(s1, s2)) return 0.0;
-  return min(min(getDistanceSP(s1, s2.p1), getDistance(s1, s2.p2)),
-	     min(getDistanceSP(s2, s1.p1), getDistance(s2, s1.p2)));
+  return min(min(getDistanceSP(s1, s2.p1), getDistanceSP(s1, s2.p2)),
+	     min(getDistanceSP(s2, s1.p1), getDistanceSP(s2, s1.p2)));
 }
+// 線分の交点
+Point getCrossPoint(Segment s1, Segment s2)
+{
+  Vector base = s2.p2 - s2.p1;
+  double d1 = abs(cross(base, s1.p1 - s2.p1));
+  double d2 = abs(cross(base, s1.p2 - s2.p1));
+  double t = d1 / (d1 + d2);
+  return s1.p1 + (s1.p2 - s1.p1) * t;
+}
+// 円cと線分lの交点
+pair<Point, Point> getCrossPoints(Circle c, Line l)
+{
+  // assert(intersect(c, l));
+  Vector pr = project(l, c.c);
+  Vector e = (l.p2 - l.p1) / abs(l.p2 - l.p1);
+  double base = sqrt(c.r*c.r - norm(pr - c.c));
+  return make_pair(pr + e * base, pr - e * base);
+}
+// 円c1と円c2の交点
+double arg(Vector p) { return atan2(p.y, p.x); }
+Vector polar(double a, double r) { return Point(cos(r) * a, sin(r) * a); }
+pair<Point, Point> getCrossPoints(Circle c1, Circle c2)
+{
+  // assert(intersect(c1, c2));
+  double d = abs(c1.c - c2.c);
+  double a = acos((c1.r*c1.r + d*d - c2.r*c2.r) / (2*c1.r*d));
+  double t = arg(c2.c - c1.c);
+  return make_pair(c1.c + polar(c1.r, t + a), c1.c + polar(c1.r, t - a));
+}
+// 点の内包
+static const int IN_POLYGON = 2;
+static const int ON_POLYGON = 1;
+static const int OUT_POLYGON = 0;
+int contains(Polygon g, Point p)
+{
+  int n = g.size();
+  bool x = false;
+  for(int i = 0; i < n; i++) {
+    Point a = g[i] - p, b = g[(i+1) % n] - p;
+    if(abs(cross(a, b)) < EPS && dot(a, b) < EPS) return ON_POLYGON;
+    if(a.y > b.y) swap(a, b);
+    if(a.y < EPS && EPS < b.y && cross(a, b) > EPS) x = !x;
+  }
+  return (x ? IN_POLYGON : OUT_POLYGON);
+}
+// 凸包
+Polygon andrewScan(Polygon s)
+{
+  Polygon u, l;
+  if(s.size() < 3) return s;
+  sort(s.begin(), s.end());
+  u.push_back(s[0]);
+  u.push_back(s[1]);
+  l.push_back(s[s.size()-1]);
+  l.push_back(s[s.size()-2]);
 
-struct Circle {
-  Point c;
-  double r;
-  Circle(Point c = Point(), double r = 0.0):c(c), r(r){}
-};
+  for(int i = 2; i < s.size(); i++) {
+    for(int n = u.size(); n >= 2 && ccw(u[n-2], u[n-1], s[i]) == COUNTER_CLOCKWISE; n--) {
+      u.pop_back();
+    }
+    u.push_back(s[i]);
+  }
 
-typedef vector<Point> Polygon;
+  for(int i = s.size()-3; i >= 0; i--) {
+    for(int n = l.size(); n >= 2 && ccw(l[n-2], l[n-1], s[i]) == COUNTER_CLOCKWISE; n--) {
+      l.pop_back();
+    }
+    l.push_back(s[i]);
+  }
+  
+  reverse(l.begin(), l.end());
+  for(int i = u.size()-2; i >= 1; i--) l.push_back(u[i]);
+
+  return l;
+}
--- template.cpp
+++ template.cpp
@@ -2,29 +2,22 @@
 
 using namespace std;
 
-using ll = long long;
-using Pi = pair<int, int>;
-using Pl = pair<ll, ll>;
-using Ti = tuple<int, int, int>;
-using Tl = tuple<ll, ll, ll>;
-
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define mt make_tuple
+#define int long long
 #define F first
 #define S second
-#define Get(t, i) get<(i)>((t))
 #define all(v) (v).begin(), (v).end()
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define reps(i, f, n) for(int i = (int)(f); i < (int)(n); i++)
 #define each(a, b) for(auto& a : b)
 
-const int inf = 1 << 25;
-const ll INF = 1LL << 55;
+typedef pair<int, int> P;
 
-int main()
+const int inf = 1LL << 55;
+
+signed main()
 {
-
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(12);
   return 0;
 }
