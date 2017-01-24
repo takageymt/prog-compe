69a0ae8e11906190e3e2738deeee0994f4d7fe04
Takuma Kageyama <uku09unk@gmail.com>
2017-01-24 11:59:56 +0900

    update

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,41 +1,10 @@
+#define MAX_N 202020
+
 typedef long long int64;
 
-const int64 mod = 1e9 + 7LL;
+const int64 mod = 1e9 + 7;
 
-int64 fact[1000010], invfact[1000010];
-
-int64 power(int64 a, int64 x)
-{
-  if(x == 0) return 1;
-  int64 ret = pow(a, x/2LL, mod);
-  ret = (ret*ret) % mod;
-  if(x & 1) ret = (ret*a) % mod;
-  return ret;
-}
-
-int64 inverse(int64 a)
-{
-  return power(a, mod-2);
-}
-
-void factorial()
-{
-  fact[0] = invfact[0] = 1LL;
-  for(int i = 1; i < 1000000; i++) {
-    fact[i] = (fact[i-1]*i) % mod;
-    invfact[i] = inverse(fact[i]);
-  }
-}
-
-int64 combination(int64 n, int64 r)
-{
-  if(r < 0 || n < r) return 0LL;
-  return (((fact[n]*invfact[r]) % mod)*invfact[n-r]) % mod;
-}
-
-//--------------------------------------------------------
-
-int64 extgcd(int64 a, int64 b, int64 &x, int64 &y)
+inline int64 extgcd(int64 a, int64 b, int64 &x, int64 &y)
 {
   int64 d = a;
   if(b != 0) d = extgcd(b, a % b, y, x), y -= (a / b) * x;
@@ -43,25 +12,44 @@ int64 extgcd(int64 a, int64 b, int64 &x, int64 &y)
   return d;
 }
 
-int64 modinv(int64 a, int64 m)
+inline int64 modPow(int64 x, int64 n)
+{
+  if(n == 0) return 1;
+  int64 ret = modPow(x, n/2);
+  (ret *= ret) % mod;
+  if(n & 1) (ret *= ret) %= mod;
+  return ret;
+}
+
+inline int64 modInv(int64 a)
+{
+  return modPow(a, mod - 2);
+}
+
+inline int64 modInv2(int64 a)
 {
   int64 x, y;
-  extgcd(a, m, x, y);
-  return (m + x % m) % m;
+  extgcd(a, mod, x, y);
+  return (m + x % mod) % mod;
 }
 
-int64 f[200020];
-void fact()
+inline int64 modFact(int n)
 {
-  f[0] = 1;
-  for(int i = 1; i < 200020; i++) {
-    f[i] = f[i-1] * i % mod;
+  int64 ret = 1;
+  while(n > 1) (ret *= n--) %= mod;
+  return ret;
+}
+
+inline int64 modCombi(int64 n, int64 r)
+{
+  static int64 fact[MAX_N], invfact[MAX_N];
+  if(fact == 0) {
+    fact[0] = invfact[0] = 1;
+    for(int i = 1; i < MAX_N; i++) {
+      fact[i] = fact[i-1] * i % mod;
+      invfact[i] = modInv(fact[i]);
+    }
   }
+  if(r < 0 || n < r) return 0;
+  return fact[n] * invfact[r] % mod * invfact[n-r] % mod;
 }
-
-int64 combi(int64 n, int64 k, int64 m)
-{
-  if(k < 0 || n < k) return 0;
-  return f[n] * modinv(f[k] * f[n - k] % m, m) % m;
-}
- 
--- Math/TwelvefoldWay.cpp
+++ Math/TwelvefoldWay.cpp
@@ -1,41 +1,4 @@
 // 写像12相
-#define MAX_N 202020
-
-typedef long long int64;
-
-const int64 mod = 1e9 + 7;
-
-inline int64 modPow(int64 x, int64 n)
-{
-  if(n == 0) return 1;
-  int64 ret = modPow(x, n/2);
-  (ret *= ret) % mod;
-  if(n & 1) (ret *= ret) %= mod;
-  return ret;
-}
-inline int64 modInv(int64 a)
-{
-  return modPow(a, mod - 2);
-}
-inline int64 modFact(int n)
-{
-  int64 ret = 1;
-  while(n > 1) (ret *= n--) %= mod;
-  return ret;
-}
-inline int64 modCombi(int64 n, int64 r)
-{
-  static int64 fact[MAX_N], invfact[MAX_N];
-  if(fact == 0) {
-    fact[0] = invfact[0] = 1;
-    for(int i = 1; i < MAX_N; i++) {
-      fact[i] = fact[i-1] * i % mod;
-      invfact[i] = modInv(fact[i]);
-    }
-  }
-  if(r < 0 || n < r) return 0;
-  return fact[n] * invfact[r] % mod * invfact[n-r] % mod;
-}
 
 // ボール:区別有 箱区別有 入れ方に制限なし
 // m^n
